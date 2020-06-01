99c63657b78ea426f2ec2d1e3c6d712c2fda13df
sihem azibi <s.azibi@live.fr>
2020-06-01 15:10:44 -0500

    store model done

---
+++ BaristaBuddyApi/Models/Store.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaristaBuddyApi.Models
+{
+    public class Store
+    {
+
+        public int Id { get; set; }
+        [Required]
+       public string Name { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+
+        public string City { get; set; }
+
+        [Required]
+        public string state { get; set; }
+        [Required]
+
+        public string Zip { get; set; }
+        
+        public string Phone { get; set; }
+
+        public string WebSiteUrl { get; set; }
+
+        public string StoreImageUrl { get; set; }
+
+       
+    }
+}
