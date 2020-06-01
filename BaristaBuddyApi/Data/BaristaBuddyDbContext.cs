72c051426b7894dc3716df18a9c6f0f238f9ae9b
sihem azibi <s.azibi@live.fr>
2020-06-01 15:53:43 -0500

    seeding store table and migration

--- BaristaBuddyApi/Data/BaristaBuddyDbContext.cs
+++ BaristaBuddyApi/Data/BaristaBuddyDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BaristaBuddyApi.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,21 @@ namespace BaristaBuddyApi.Data
 {
     public class BaristaBuddyDbContext : DbContext 
     {
+        public BaristaBuddyDbContext (DbContextOptions options) : base(options)
+        {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Store>()
+                .HasData(
+                new Store { Id = 1, City = "iowa city", state = "iowa", Name = "matt's Place", StreetAddress = "1918 H st", Zip = "52240" }
+                
+                );
+
+        }
+
+        public DbSet<Store> Store { get; set; }
     }
 }
