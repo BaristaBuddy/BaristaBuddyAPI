2e89ce5a1c3e4f742ad044ac1db6f1fbe119bf9d
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-01 18:30:57 -0500

    removed whitespace

--- BaristaBuddyApi/Models/Store.cs
+++ BaristaBuddyApi/Models/Store.cs
@@ -8,30 +8,19 @@ namespace BaristaBuddyApi.Models
 {
     public class Store
     {
-
         public int Id { get; set; }
         [Required]
        public string Name { get; set; }
-
         [Required]
         public string StreetAddress { get; set; }
-
         [Required]
-
         public string City { get; set; }
-
         [Required]
         public string State { get; set; }
         [Required]
-
         public string Zip { get; set; }
-        
         public string Phone { get; set; }
-
         public string WebsiteUrl { get; set; }
-
         public string StoreImageUrl { get; set; }
-
-       
     }
 }
