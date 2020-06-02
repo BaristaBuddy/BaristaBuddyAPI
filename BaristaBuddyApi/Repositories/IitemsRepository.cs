c3dc6cd8c6525c4128da64288441949a27e69405
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-02 16:24:12 -0500

    added Iitemsrepo and itemrepo

---
+++ BaristaBuddyApi/Repositories/IitemsRepository.cs
@@ -0,0 +1,12 @@
+﻿using BaristaBuddyApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaristaBuddyApi.Repositories
+{
+    public interface IitemsRepository
+    {
+        Task<IEnumerable<Item>> GetAllItems();
+    }
+}
---
+++ BaristaBuddyApi/Repositories/ItemsRepository.cs
@@ -0,0 +1,26 @@
+﻿using BaristaBuddyApi.Data;
+using BaristaBuddyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BaristaBuddyApi.Repositories
+{
+    
+
+    
+    public class ItemsRepository : IitemsRepository
+    {
+        private BaristaBuddyDbContext _context;
+
+        public ItemsRepository(BaristaBuddyDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        public Task<IEnumerable<Item>> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
