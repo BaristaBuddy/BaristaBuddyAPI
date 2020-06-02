14a76bdf9f2ca8dcc2714fde56d91c350f8db58d
Keith Dahlby <keith@newbo.co>
2020-06-02 14:27:05 -0500

    Remove redundant Store migration

--- BaristaBuddyApi/Migrations/20200602162127_First.cs
+++ BaristaBuddyApi/Migrations/20200602162127_First.cs
@@ -24,34 +24,6 @@ namespace BaristaBuddyApi.Migrations
                     table.PrimaryKey("PK_Item", x => new { x.ItemId, x.StoreId });
                 });
 
-            migrationBuilder.CreateTable(
-                name: "Store",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(nullable: false),
-                    StreetAddress = table.Column<string>(nullable: false),
-                    City = table.Column<string>(nullable: false),
-                    State = table.Column<string>(nullable: false),
-                    Zip = table.Column<string>(nullable: false),
-                    Phone = table.Column<string>(nullable: true),
-                    WebsiteUrl = table.Column<string>(nullable: true),
-                    StoreImageUrl = table.Column<string>(nullable: true),
-                    ItemId = table.Column<int>(nullable: true),
-                    ItemStoreId = table.Column<int>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Store", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Store_Item_ItemId_ItemStoreId",
-                        columns: x => new { x.ItemId, x.ItemStoreId },
-                        principalTable: "Item",
-                        principalColumns: new[] { "ItemId", "StoreId" },
-                        onDelete: ReferentialAction.Restrict);
-                });
-
             migrationBuilder.InsertData(
                 table: "Item",
                 columns: new[] { "ItemId", "StoreId", "ImageUrl", "Ingredients", "Name", "Price" },
@@ -62,28 +34,10 @@ namespace BaristaBuddyApi.Migrations
                     { 3, 2, null, null, "Sihem's Caramel Latte", 4.0 },
                     { 4, 3, null, "Enough caffiene to kill a small horse", "James's Triple Death", 6.6600000000000001 }
                 });
-
-            migrationBuilder.InsertData(
-                table: "Store",
-                columns: new[] { "Id", "City", "ItemId", "ItemStoreId", "Name", "Phone", "State", "StoreImageUrl", "StreetAddress", "WebsiteUrl", "Zip" },
-                values: new object[,]
-                {
-                    { 1, "Iowa City", null, null, "Matt's Place", null, "Iowa", null, "1918 H St.", null, "52240" },
-                    { 2, "Cedar Rapids", null, null, "Roastopia", null, "Iowa", null, "1313 1st Ave.", null, "52240" },
-                    { 3, "Des Moines", null, null, "King Keith's Cafe", null, "Iowa", null, "121 Dodge St.", null, "52240" }
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Store_ItemId_ItemStoreId",
-                table: "Store",
-                columns: new[] { "ItemId", "ItemStoreId" });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Store");
-
             migrationBuilder.DropTable(
                 name: "Item");
         }
