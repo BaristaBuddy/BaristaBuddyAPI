fc64db3d238f16e682c590af86171ae404f9dd86
Keith Dahlby <keith@newbo.co>
2020-06-02 14:52:39 -0500

    Revert "Reset DbContext"
    This reverts commit eeb0d2e08801909f924d85ebe2dde58e2c497e28.

--- BaristaBuddyApi/Data/BaristaBuddyDbContext.cs
+++ BaristaBuddyApi/Data/BaristaBuddyDbContext.cs
@@ -17,6 +17,77 @@ namespace BaristaBuddyApi.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Store>()
+                .HasData(
+                new Store 
+                {
+                    Id = 1,
+                    City = "Iowa City",
+                    State = "Iowa",
+                    Name = "Matt's Place",
+                    StreetAddress = "1918 H St.",
+                    Zip = "52240" 
+                },
+                new Store
+                {
+                    Id = 2,
+                    City = "Cedar Rapids",
+                    State = "Iowa",
+                    Name = "Roastopia",
+                    StreetAddress = "1313 1st Ave.",
+                    Zip = "52240"
+                },
+                new Store
+                {
+                    Id = 3,
+                    City = "Des Moines",
+                    State = "Iowa",
+                    Name = "King Keith's Cafe",
+                    StreetAddress = "121 Dodge St.",
+                    Zip = "52240"
+                });
+            modelBuilder.Entity<Item>()
+                .HasKey(Item => Item.ItemId);
+
+            modelBuilder.Entity<Item>()
+                .HasData(
+                new Item { ItemId = 1, StoreId = 1, Name = "Brennan's Hot Chocolate", Ingredients = "Almond Milk, Cocoa nibs, Honey, Cinnamon", Price = 3.13 },
+                new Item { ItemId = 2, StoreId = 1, Name = "Matt's Espresso Macchiato", Price = 4.17 },
+                new Item { ItemId = 3, StoreId = 2, Name = "Sihem's Caramel Latte", Price = 4.00 },
+                new Item { ItemId = 4, StoreId = 3, Name = "James's Triple Death", Ingredients = "Enough caffiene to kill a small horse", Price = 6.66 }
+                );
+
+            modelBuilder.Entity<StoreModifier>()
+                .HasKey(StoreModifier => StoreModifier.ModifierId);
+
+            modelBuilder.Entity<StoreModifier>()
+                .HasData(
+                new StoreModifier { ModifierId = 1, StoreId = 1, Name = "Mint" },
+                new StoreModifier { ModifierId = 2, StoreId = 1, Name = "Espresso Shot" },
+                new StoreModifier { ModifierId = 3, StoreId = 2, Name = "Caramel" }
+                );
+
+            modelBuilder.Entity<ItemModifier>()
+                .HasKey(ItemModifier => new
+                {
+                    ItemModifier.ItemId,
+                    ItemModifier.ModifierId
+                });
+
+            modelBuilder.Entity<ItemModifier>()
+                .HasData(
+                new ItemModifier { ModifierId = 1, ItemId = 1, AdditionalCost = 0.75 },
+                new ItemModifier { ModifierId = 2, ItemId = 4, AdditionalCost = 1.50 },
+                new ItemModifier { ModifierId = 3, ItemId = 3, AdditionalCost = 0.75 }
+                );
+
         }
+
+        public DbSet<Store> Store { get; set; }
+        public DbSet<Item> Item { get; set; }
+
+        public DbSet<StoreModifier> StoreModifier { get; set; }
+
+        public DbSet<ItemModifier> itemModifier { get; set; }
     }
 }
