b5ecc3a6ee5673745b138a69d574fa2477eba202
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-03 14:54:48 -0500

    different changes to itemModifierDTO

--- BaristaBuddyApi/Models/DTO/ItemModifierDTO.cs
+++ BaristaBuddyApi/Models/DTO/ItemModifierDTO.cs
@@ -8,11 +8,13 @@ namespace BaristaBuddyApi.Models.DTO
 {
     public class ItemModifierDTO
     {
-      
+        // PK, FK to Item
+        //public int ItemId { get; set; }
+       // public Item Item { get; set; }
 
         // PK, FK to StoreModifier
-        public int ModifierId { get; set; }
-        public StoreModifier Modifier { get; set; }
+        //public int ModifierId { get; set; }
+        //public StoreModifier Modifier { get; set; }
 
         [Column(TypeName = "money")]
         public decimal AdditionalCost { get; set; }
