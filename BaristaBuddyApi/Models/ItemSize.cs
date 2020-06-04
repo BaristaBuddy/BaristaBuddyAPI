5c327bf7649df4d83110ee70aa56dea32a991e6a
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-03 20:28:49 -0500

    added model for Itemsize

---
+++ BaristaBuddyApi/Models/ItemSize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaristaBuddyApi.Models
+{
+    public class ItemSize
+    {
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+
+        public string Size { get; set; }
+
+        public decimal AdditionalCost { get; set; }
+    }
+}
