ca2f8fd3816405c00b9af23d7da1fb5c0d794e6e
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-03 20:42:51 -0500

    added migration

--- BaristaBuddyApi/Migrations/20200604014124_AddItemSizeTable.Designer.cs
+++ BaristaBuddyApi/Migrations/20200604014124_AddItemSizeTable.Designer.cs
@@ -3,14 +3,16 @@ using BaristaBuddyApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BaristaBuddyApi.Migrations
 {
     [DbContext(typeof(BaristaBuddyDbContext))]
-    partial class BaristaBuddyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200604014124_AddItemSizeTable")]
+    partial class AddItemSizeTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
