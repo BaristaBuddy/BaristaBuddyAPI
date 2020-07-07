7446eb882e9ec4092fd18bb46977fc3e70a941e9
sihem azibi <s.azibi@live.fr>
2020-07-06 19:24:30 -0500

    create table loginData

--- BaristaBuddyApi/Controllers/UserController.cs
+++ BaristaBuddyApi/Controllers/UserController.cs
@@ -1,10 +1,12 @@
 ﻿
 using BaristaBuddyApi.Models.Identity;
 using BaristaBuddyApi.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace BaristaBuddyApi.Controllers
@@ -21,7 +23,130 @@ namespace BaristaBuddyApi.Controllers
             {
                 this.userManager = userManager;
             }
+            [HttpPost("Login")]
+            public async Task<IActionResult> Login(BaristaBuddyUser login)
+            {
+                var user = await userManager.FindByNameAsync(login.UserName);
+                if (user != null)
+                {
+                    var result = await userManager.CheckPasswordAsync(user, login.Password);
+                    if (result)
+                    {
+                        return Ok(new BaristaBuddyUser
+                        {
+                            UserId = user.Id,
+                            Token = userManager.CreateToken(user)
+
+                        });
+                    }
+
+                    await userManager.AccessFailedAsync(user);
+                }
+                return Unauthorized();
+            }
+
+            [HttpPost("Register")]
+
+            public async Task<IActionResult> Register(BaristaBuddyUser register)
+            {
+                var user = new BaristaBuddyUser
+                {
+                    Email = register.Email,
+                    UserName = register.Email,
+                    BirthDate= register.BirthDate,
+                    PhoneNumber=register.PhoneNumber,
+                    FirstName = register.FirstName,
+                    LastName = register.LastName
+
+                };
+
+                var result = await userManager.CreateAsync(user, register.Password);
+                if (!result.Succeeded)
+                {
+                    return BadRequest(new
+                    {
+                        message = "registration failed",
+                        errors = result.Errors
+                    });
+                }
+
+                return Ok(new BaristaBuddyUser
+                {
+                    UserId = user.Id,
+                    Token = userManager.CreateToken(user)
+                });
+
+
+            }
+
+            [Authorize]
+            [HttpGet("Self")]
+            public async Task<IActionResult> Self()
+            {
+                if (HttpContext.User.Identity is ClaimsIdentity identity)
+                {
+                    var usernameClaim = identity.FindFirst("UserId");
+                    var userId = usernameClaim.Value;
+
+                    var user = await userManager.FindByIdAsync(userId);
+                    if (user == null)
+                    {
+                        return Unauthorized();
+                    }
+
+                    return Ok(new
+                    {
+                        UserId = user.Id,
+                        user.Email,
+                        user.FirstName,
+                        user.LastName
+
+                    });
+                }
+                return Unauthorized();
+            }
+
+            [HttpGet("{userId}")]
+            public async Task<IActionResult> GetUser(string userId)
+            {
+                var user = await userManager.FindByIdAsync(userId);
+                if (user == null)
+                    return NotFound();
+
+                return Ok(new
+                {
+                    UserId = user.Id,
+                    user.Email,
+                    user.FirstName,
+                    user.LastName,
+
+                });
+            }
+
+            [HttpPut("userId")]
+
+            public async Task<IActionResult> UpdateUser(string userId, BaristaBuddyUser data)
+            {
+                var user = await userManager.FindByIdAsync(userId);
+                if (user == null)
+                    return NotFound();
+
+                user.FirstName = data.FirstName;
+                user.LastName = data.LastName;
+
+                await userManager.UpdateAsync(user);
+
+                return Ok(new
+                {
+                    UserId = user.Id,
+                    user.Email,
+                    user.FirstName,
+                    user.LastName,
+
+                });
+            }
 
         }
     }
-}
+    }
+
--- BaristaBuddyApi/Models/Identity/BaristaBuddyUser.cs
+++ BaristaBuddyApi/Models/Identity/BaristaBuddyUser.cs
@@ -7,18 +7,15 @@ using System.Threading.Tasks;
 
 namespace BaristaBuddyApi.Models.Identity
 {
-    public class BaristaBuddyUser:IdentityUser
+    public class BaristaBuddyUser : IdentityUser
     {
         [Required]
+
         public string FirstName { get; set; }
         [Required]
-        public string LastName { get; set;}
-        public string PhoneNumber { get; set;}
-        public DateTime? BirthDate { get; set;}
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
         public string ImageUrl { get; set; }
-        [Required]
-        public string Email { get; set; }
-        [Required]
-        public string Password { get; set; }
+
     }
 }
