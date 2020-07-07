d5bab789e49ef5ab8881ee9cb9312568e0f88668
sihem azibi <s.azibi@live.fr>
2020-07-07 12:13:14 -0500

    IOrderRepository done

---
+++ BaristaBuddyApi/Repositories/IOrdersRepository.cs
@@ -0,0 +1,17 @@
+﻿using BaristaBuddyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaristaBuddyApi.Repositories
+{
+    public class IOrdersRepository
+    {
+        Task<IEnumerable<Orders>> GetAllOrders();
+        Task<Orders> GetOneOrder(int id);
+        Task<bool> UpdateOrder(int id, Orders order);
+        Task<Orders> SaveNew(Orders order);
+        Task<Orders> DeleteOrder(int id);
+    }
+}
---
+++ BaristaBuddyApi/Repositories/OrdersRepository.cs
@@ -0,0 +1,13 @@
+﻿using BaristaBuddyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaristaBuddyApi.Repositories
+{
+    public class OrdersRepository
+    {
+       
+    }
+}
