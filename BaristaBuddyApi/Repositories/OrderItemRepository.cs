99ecfe38d26c0a2ba86019049843a300d59c6e35
sihem azibi <s.azibi@live.fr>
2020-07-08 10:50:55 -0500

    ceate OrderItemController

--- BaristaBuddyApi/Repositories/OrderItemRepository.cs
+++ BaristaBuddyApi/Repositories/OrderItemRepository.cs
@@ -1,11 +1,41 @@
-﻿using System;
+﻿using BaristaBuddyApi.Data;
+using BaristaBuddyApi.Models.DTO;
+using BaristaBuddyApi.Models.Identity;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BaristaBuddyApi.Repositories
 {
-    public class OrderItemRepository
+    public class OrderItemRepository : IOrderItemRepository
     {
+        private BaristaBuddyDbContext _context;
+
+        public OrderItemRepository (BaristaBuddyDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        public async Task<OrderItemDTO> CreateNewOrderItem(OrderItem orderItem)
+        {
+            _context.OrderItem.Add(orderItem);
+
+            await _context.SaveChangesAsync();
+
+            return await GetOneOrder(orderItem.Id);
+
+        }
+
+        public Task<OrderItemDTO> DeleteOrderItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UptadeOrderItem(int id, OrderItem orderItem)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
