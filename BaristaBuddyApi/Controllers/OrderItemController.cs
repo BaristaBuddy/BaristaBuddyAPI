6ab1333e93a98cd4d509224d9c5b7e716c82d75c
sihem azibi <s.azibi@live.fr>
2020-07-08 11:58:47 -0500

    GetOneOrderItem controller ready for test

--- BaristaBuddyApi/Controllers/OrderItemController.cs
+++ BaristaBuddyApi/Controllers/OrderItemController.cs
@@ -1,4 +1,7 @@
 ﻿using BaristaBuddyApi.Data;
+using BaristaBuddyApi.Models;
+using BaristaBuddyApi.Models.DTO;
+using BaristaBuddyApi.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,7 +10,28 @@ using System.Threading.Tasks;
 
 namespace BaristaBuddyApi.Controllers
 {
+    [Route("api/Order/Item")]
+    [ApiController]
     public class OrderItemController : ControllerBase
     {
+        IOrderItemRepository orderItemRepository;
+        
+        public OrderItemController ( IOrderItemRepository orderItemRepository)
+        {
+            this.orderItemRepository = orderItemRepository;
+        }
+
+        [HttpGet("{id}")]
+        public async  Task<ActionResult<OrderItemDTO> > GetOrderItem (int id )
+        {
+            var orderItem = await orderItemRepository.GetOneOrderItem(id);
+
+            if (orderItem == null)
+            {
+                return NotFound();
+            }
+
+            return orderItem;
+        }
     }
 }
