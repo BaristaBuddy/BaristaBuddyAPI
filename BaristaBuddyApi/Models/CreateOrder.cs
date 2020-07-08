40b22db5f9803eb0e7a5aecbc6311a0b17760f4d
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-08 12:00:49 -0500

    created  dto for creating order, not much in there at the moment

---
+++ BaristaBuddyApi/Models/CreateOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaristaBuddyApi.Models
+{
+    public class CreateOrder
+    {
+        public int Id { get; set; }
+        public int storeId { get; set; }
+    }
+}
